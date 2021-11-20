3254d3343a3361eb52a965c45d62e7228356405e
Kelvin <kelvin0898@live.com>
2021-11-20 11:37:52 +1100

    changing from white color to null instead for the background color

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function makeTable(rows, cols) {
   };
 };
 
-makeTable(50, 50);
+makeTable(16, 16);
 
 // const container = document.querySelector('.container')
 const grids = document.querySelectorAll('.table')
 grids.forEach(element => {
-  element.addEventListener('mouseenter', (e) => {
+  element.addEventListener('mouseover', (e) => {
     e.target.style.backgroundColor = randomColor();
-    console.log(e)
+    // console.log(e)
   })
 });
 
@@ -30,3 +30,15 @@ function randomColor() {
   var generateColor = '#' + Math.floor(Math.random()*16777215).toString(16);
   return generateColor;
 }
+
+function resizeGrid() {
+  sketchSize = prompt("Enter 1 to 100 to resize sketch");
+  return sketchSize;
+}
+
+const clear = document.querySelector('.clear-button')
+clear.addEventListener('click', () => {
+  grids.forEach(element => {
+    element.style.backgroundColor = null;
+  })
+})
